577140daf8fc28372eb39531dc8792cc8a7e6080
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-09-16 20:49:47 -0500

    Name with code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -65,7 +68,10 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         ) {
             Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
-        Text(text = stringResource(R.string.nameAutor), fontSize = 30.sp)
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        Text(text = "Laura Daniela Jaimes Cárdenas 2040430", fontSize = 10.sp )
+
     }
 }
 
